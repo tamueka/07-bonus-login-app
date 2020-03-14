f5cd80d49aebd722608e6a3af9e57b21e3f6fbdb
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-03-14 19:03:09 +0100

    pantalla login y sus validaciones + añadimos animated las vistas login y registro + configuramos logica en login.ts + validamos campos y conecamos con el modelo

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioModel;
+
+  constructor() {}
 
   ngOnInit() {
+
+    this.usuario = new UsuarioModel();
+
+  }
+
+  login( form: NgForm ){
+
+    if ( form.invalid ) {return;}
+
+    console.log('Usuario Enviado');
+    console.log(this.usuario);
+    console.log(form)
+
   }
 
 }
