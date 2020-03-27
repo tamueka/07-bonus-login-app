4fca0e0e190a98085e546006e09e8bb057b2fc24
tamueka <tamueka@hotmail.com>
2020-03-27 13:40:55 +0100

    arreglamos conflictos

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from 'src/app/models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,7 +12,7 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -21,10 +22,11 @@ export class RegistroComponent implements OnInit {
 
     if ( form.invalid ){ return; };
 
-    console.log('Usuario Enviado');
-    console.log(this.usuario);
-    console.log(form)
+    this.auth.nuevoUsuario(this.usuario).subscribe(resp => {
+      console.log(resp)
+  })
+
   }
 
-
 }
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UsuarioModel } from '../models/usuario.model';
@@ -7,17 +8,15 @@ import { UsuarioModel } from '../models/usuario.model';
 })
 export class AuthService {
 
-  private url: 'https://identitytoolkit.googleapis.com/v1/accounts:';
-  private apiKey: 'AIzaSyD0yqAGw7BEA_1Mk9wy8S8b9x_zJgpfAMg';
+  public url: 'https://identitytoolkit.googleapis.com/v1';
+  public apiKey: 'AIzaSyD0yqAGw7BEA_1Mk9wy8S8b9x_zJgpfAMg';
+
 
 //crear nuevo usuario:
 //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
 
 //login
 //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
-
-
-
 
   constructor( private http: HttpClient ) { }
 
@@ -31,6 +30,17 @@ export class AuthService {
 
     nuevoUsuario( usuario: UsuarioModel ){
 
+      const authData = {
+/*         email: usuario.email,
+        password: usuario.password, */
+
+        ...usuario,
+        returnSecureToken: true
+      };
+
+      return this.http.post(
+                              `${this.url}/accounts:signUp?key=${this.apiKey}`,
+                              authData);
     }
 
 }
