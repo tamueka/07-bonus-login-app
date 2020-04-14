a50297b583f631ed4563ac656a07d676fbc0e540
tamueka <tamueka@hotmail.com>
2020-04-14 18:37:07 +0200

    instalamos sweetalert2 para mostrar mensajes  con modales al usuario + lo realizamos en los componentes login y registro

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { UsuarioModel } from "../../models/usuario.model";
 import { AuthService } from "src/app/services/auth.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   usuario: UsuarioModel;
@@ -22,12 +23,25 @@ export class LoginComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      allowOutsideClick: false,
+      text: "Espere por favor ...",
+      icon: "info",
+    });
+    Swal.showLoading();
+
     this.auth.login(this.usuario).subscribe(
-      resp => {
+      (resp) => {
         console.log(resp);
+        Swal.close();
       },
-      err => {
+      (err) => {
         console.log(err.error.error.message);
+        Swal.fire({
+          title: "Error al autenticar",
+          text: err.error.error.message,
+          icon: "error",
+        });
       }
     );
   }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,40 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { UsuarioModel } from 'src/app/models/usuario.model';
-import { NgForm } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { UsuarioModel } from "src/app/models/usuario.model";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  selector: "app-registro",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.css"],
 })
 export class RegistroComponent implements OnInit {
-
   usuario: UsuarioModel;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService) {}
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
 
-    if ( form.invalid ){ return; };
+    Swal.fire({
+      allowOutsideClick: false,
+      text: "Espere por favor ...",
+      icon: "info",
+    });
+    Swal.showLoading();
 
-    this.auth.nuevoUsuario( this.usuario ).subscribe(
-      resp => {
-
-        console.log(resp)
-
-      },err => {
-
+    this.auth.nuevoUsuario(this.usuario).subscribe(
+      (resp) => {
+        console.log(resp);
+        Swal.close();
+      },
+      (err) => {
         console.log(err.error.error.message);
-
+        Swal.fire({
+          title: "Error al autenticar",
+          text: err.error.error.message,
+          icon: "error",
+        });
       }
-    )
-
+    );
   }
-
 }
-
