806a17e6bdcc3cba9fc145d6002d6b671e1850bd
tamueka <tamueka@hotmail.com>
2020-04-14 20:30:09 +0200

    mejoramos la validacion del token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import { UsuarioModel } from "../models/usuario.model";
 import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
 export class AuthService {
   public url: "https://identitytoolkit.googleapis.com/v1";
@@ -23,23 +23,24 @@ export class AuthService {
   }
 
   logout() {
-    localStorage.removeItem('token')
+    localStorage.removeItem("token");
   }
 
   login(usuario: UsuarioModel) {
     const authData = {
-      ...usuario,         //spread operator, operadores spread
-      returnSecureToken: true
+      ...usuario, //spread operator, operadores spread
+      returnSecureToken: true,
     };
 
     return this.http
       .post(
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD0yqAGw7BEA_1Mk9wy8S8b9x_zJgpfAMg",
         authData
-      ).pipe(
+      )
+      .pipe(
         map((resp) => {
           console.log("Entro en el mapa de rxjs");
-          this.guardarToken( resp["idToken"] );
+          this.guardarToken(resp["idToken"]);
           return resp;
         })
       );
@@ -47,41 +48,57 @@ export class AuthService {
 
   nuevoUsuario(usuario: UsuarioModel) {
     const authData = {
-      ...usuario,         //spread operator, operadores spread
+      ...usuario, //spread operator, operadores spread
       returnSecureToken: true,
     };
 
-    return this.http.post(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0yqAGw7BEA_1Mk9wy8S8b9x_zJgpfAMg",
-      authData
-    ).pipe(
-      map( resp =>{
-        console.log("Entro en el mapa de rxjs")
-        this.guardarToken( resp['idToken'] );
-        return resp;
-      })
-    );
+    return this.http
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD0yqAGw7BEA_1Mk9wy8S8b9x_zJgpfAMg",
+        authData
+      )
+      .pipe(
+        map((resp) => {
+          console.log("Entro en el mapa de rxjs");
+          this.guardarToken(resp["idToken"]);
+          return resp;
+        })
+      );
   }
 
-  private guardarToken(idToken: string){
+  private guardarToken(idToken: string) {
     this.userToken = idToken;
 
-    localStorage.setItem('token', idToken);
+    localStorage.setItem("token", idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+
+    localStorage.setItem("expira", hoy.getTime().toString());
   }
 
-  leerToken(){
-    if( localStorage.getItem('token') ){
-      this.userToken = localStorage.getItem('token')
+  leerToken() {
+    if (localStorage.getItem("token")) {
+      this.userToken = localStorage.getItem("token");
+    } else {
+      this.userToken = "";
     }
-    else{
-        this.userToken = '';
-      }
-      return this.userToken;
+    return this.userToken;
+  }
+
+  estaAutenticado(): boolean {
+    if (this.userToken.length < 2) {
+      return false;
     }
 
-    estaAutenticado():boolean{
-      return this.userToken.length > 2;
-    }
+    const expira = Number(localStorage.getItem("expira"));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
 
+    if (expiraDate > new Date()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
-
